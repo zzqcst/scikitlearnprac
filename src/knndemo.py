0906cbce4a19f9d509be33c24ce412745342f1ff
zhi <zzqlrr@gmail.com>
2018-10-22 22:29:39 +0800

    sync

--- src/knndemo.py
+++ src/knndemo.py
@@ -1,7 +1,13 @@
 from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
+import numpy as np
 # 生成数据
 centers = [[-2,2],[2,2],[0,4]]
 '''
 生成60个训练样本，分布在以centers参数指定中心点周围，cluster_std是标准差，生成的训练数据集X，类别标记y
 '''
 X,y=make_blobs(n_samples=60,centers=centers,random_state=0,cluster_std=0.60)
+plt.figure(figsize=(10,20),dpi=144)
+c=np.array(centers)
+plt.scatter(X[:,0],X[:,1],c=y,s=100,cmap='cool')
+plt.show()
