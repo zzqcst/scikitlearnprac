7b2bea37fa792c0b388827deb2c39e15a65bfdff
zhi <zzqlrr@gmail.com>
2018-10-23 14:01:03 +0800

    sync

--- src/knndemo.py
+++ src/knndemo.py
@@ -9,7 +9,26 @@ centers = [[-2,2],[2,2],[0,4]]
 X,y=make_blobs(n_samples=60,centers=centers,random_state=0,cluster_std=0.60)
 print(X)
 print(y)
-plt.figure(figsize=(10,20),dpi=144)
+
+
+
+from sklearn.neighbors import KNeighborsClassifier
+# 模型训练
+k=5
+clf=KNeighborsClassifier(n_neighbors=k)
+clf.fit(X,y)
+
+# 进行预测
+X_sample=[[2,2]]
+y_sample=clf.predict(X_sample)
+print(y_sample[0])
+'''
+print(neighbors)
+[[16 20 48  6 23]]
+'''
+neighbors = clf.kneighbors(X_sample,return_distance=False) # 取出来的点是训练样本X里的索引，从0开始
+
+plt.figure(figsize=(16,10),dpi=144)
 c=np.array(centers)
 '''
 s:marker size
@@ -18,4 +37,7 @@ cmap:颜色映射表
 '''
 plt.scatter(X[:,0],X[:,1],c=y,s=100,cmap='cool')
 plt.scatter(c[:,0],c[:,1],s=100,marker="^",c="orange")
+plt.scatter(X_sample[0][0],X_sample[0][1],marker='x',c=np.asarray(y_sample[0]),cmap='cool',s=100)
+for i in neighbors[0]: #遍历索引
+    plt.plot([X[i][0],X_sample[0][0]],[X[i][1],X_sample[0][1]],'k--',linewidth=0.6) # 预测点与距离最近的5个样本的连线
 plt.show()
--- src/sldemo.py
+++ src/sldemo.py
@@ -3,26 +3,26 @@ import matplotlib.pyplot as plt
 
 digits = datasets.load_digits()
 # print(digits.images[0])
-# images_and_labels = list(zip(digits.images,digits.target))
+images_and_labels = list(zip(digits.images,digits.target))
 plt.figure(figsize=(8, 6), dpi=200)
-# '''enumerate函数
-# >>>seasons = ['Spring', 'Summer', 'Fall', 'Winter']
-# >>> list(enumerate(seasons))
-# [(0, 'Spring'), (1, 'Summer'), (2, 'Fall'), (3, 'Winter')]
-# '''
-# for index,(image,label) in enumerate(images_and_labels[:8]):
-#     plt.subplot(2,4,index+1) # 2行4列，subplot的index从1开始
-#     # plt.axis('off')
-#     plt.imshow(image,cmap='gray_r',interpolation='nearest') # 绘制灰度图，gray_r中的r表示reverse
-#     plt.title('digit:%i' % label,fontsize=20)
-# plt.show()
-# from sklearn.datasets import load_digits
-# digits = load_digits()
-# print(digits.data.shape)
-# import matplotlib.pyplot as plt #doctest: +SKIP
-# plt.gray() #doctest: +SKIP
-# plt.matshow(digits.images[0]) #doctest: +SKIP
-# plt.show() #doctest: +SKIP
+'''enumerate函数
+>>>seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+>>> list(enumerate(seasons))
+[(0, 'Spring'), (1, 'Summer'), (2, 'Fall'), (3, 'Winter')]
+'''
+for index,(image,label) in enumerate(images_and_labels[:8]):
+    plt.subplot(2,4,index+1) # 2行4列，subplot的index从1开始
+    # plt.axis('off')
+    plt.imshow(image,cmap='gray_r',interpolation='nearest') # 绘制灰度图，gray_r中的r表示reverse
+    plt.title('digit:%i' % label,fontsize=20)
+plt.show()
+from sklearn.datasets import load_digits
+digits = load_digits()
+print(digits.data.shape)
+import matplotlib.pyplot as plt #doctest: +SKIP
+plt.gray() #doctest: +SKIP
+plt.matshow(digits.images[0]) #doctest: +SKIP
+plt.show() #doctest: +SKIP
 
 from sklearn.model_selection import train_test_split
 
@@ -37,32 +37,31 @@ from sklearn import svm
 clf = svm.SVC(gamma=0.001, C=100.)
 clf.fit(Xtrain, Ytrain)
 
-# # 模型测试
-# # 评估模型的准确度
-# from sklearn.metrics import accuracy_score
-# #Perform classification on samples in X.
-# Ypred = clf.predict(Xtest)
-# print(Ypred)
-# print(accuracy_score(Ytest, Ypred))
-#
-# print(clf.score(Xtest, Ytest))
-#
-# fig, axes = plt.subplots(4, 4, figsize=(8, 8))# 4行4列,每个大小8*8
-# fig.subplots_adjust(hspace=0.1, wspace=0.1)
-#
-# for i, ax in enumerate(axes.flat):
-#     ax.imshow(Xtest[i].reshape(8, 8), cmap=plt.cm.gray_r, interpolation='nearest')
-#     ax.text(0.05, 0.05, str(Ypred[i]), fontsize=32, transform=ax.transAxes,
-#             color='green' if Ypred[i] == Ytest[i] else 'red')
-#     ax.text(0.8, 0.05, str(Ytest[i]), fontsize=32, transform=ax.transAxes, color='black')
-#     ax.set_xticks([])
-#     ax.set_yticks([])
-# plt.show()
+# 模型测试
+# 评估模型的准确度
+from sklearn.metrics import accuracy_score
+#Perform classification on samples in X.
+Ypred = clf.predict(Xtest)
+print(Ypred)
+print(accuracy_score(Ytest, Ypred))
+
+print(clf.score(Xtest, Ytest))
+
+fig, axes = plt.subplots(4, 4, figsize=(8, 8))# 4行4列,每个大小8*8
+fig.subplots_adjust(hspace=0.1, wspace=0.1)
+
+for i, ax in enumerate(axes.flat):
+    ax.imshow(Xtest[i].reshape(8, 8), cmap=plt.cm.gray_r, interpolation='nearest')
+    ax.text(0.05, 0.05, str(Ypred[i]), fontsize=32, transform=ax.transAxes,
+            color='green' if Ypred[i] == Ytest[i] else 'red')
+    ax.text(0.8, 0.05, str(Ytest[i]), fontsize=32, transform=ax.transAxes, color='black')
+    ax.set_xticks([])
+    ax.set_yticks([])
+plt.show()
 
 # 保存模型参数
 from sklearn.externals import joblib
-# import numpy as np
-# joblib.dump(clf,"dig_svm.pkl")
+joblib.dump(clf,"dig_svm.pkl")
 
 # 导入模型参数，直接进行预测
 clf=joblib.load("dig_svm.pkl")
