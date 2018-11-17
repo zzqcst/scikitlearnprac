875daed3b7974f0a4cf83dc8d27a6749bf10ff61
zhiqiang zhu <zzqlrr@gmail.com>
2018-11-17 20:45:13 +0800

    sync

--- src/knndemo.py
+++ src/knndemo.py
@@ -9,7 +9,8 @@ centers = [[-2,2],[2,2],[0,4]]
 X,y=make_blobs(n_samples=60,centers=centers,random_state=0,cluster_std=0.60)
 print(X)
 print(y)
-
+ 
+    
 
 
 from sklearn.neighbors import KNeighborsClassifier
